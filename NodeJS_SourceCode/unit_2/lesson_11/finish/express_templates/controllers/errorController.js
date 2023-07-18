7a2ae1d66858eb1b59703aa508310a12b8019da3
Lerish-vZ <lerishavz@gmail.com>
2023-07-18 13:21:28 +0200

    update 18/07

--- NodeJS_SourceCode/unit_2/lesson_11/finish/express_templates/controllers/errorController.js
+++ NodeJS_SourceCode/unit_2/lesson_11/finish/express_templates/controllers/errorController.js
@@ -10,7 +10,9 @@ exports.logErrors = (error, req, res, next) => {
 exports.respondNoResourceFound = (req, res) => {
   let errorCode = httpStatus.NOT_FOUND;
   res.status(errorCode);
-  res.send(`${errorCode} | The page does not exist!`);
+  res.sendFile(`./public/${errorCode}.html`, {
+    root: "./",
+  });
 };
 
 exports.respondInternalError = (error, req, res, next) => {
