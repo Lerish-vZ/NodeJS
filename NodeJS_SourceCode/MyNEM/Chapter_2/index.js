c59c7b3c44f7d4048d40b910953ad3f99d18cdd8
Lerish-vZ <lerishavz@gmail.com>
2023-09-01 11:56:52 +0200

    Update

--- NodeJS_SourceCode/MyNEM/Chapter_2/index.js
+++ NodeJS_SourceCode/MyNEM/Chapter_2/index.js
@@ -1,1 +1,18 @@
-const express = require('express');
+const path = require('path');
+const express = require('express'); //require express module
+const app = express(); //calls express function to start new Express app
+
+app.listen(3000, () => {
+    console.log("App listening on port 3000");
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(_dirname, 'index.html'));
+})
+
+app.get('/about', (req, res) => {
+    res.json({
+        name: 'Greg Lim'
+    })
+})
+
