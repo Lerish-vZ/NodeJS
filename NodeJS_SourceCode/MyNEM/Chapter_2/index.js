132d85b444fc8fe2554cd0310eb92d1e44dad606
Lerish-vZ <lerishavz@gmail.com>
2023-09-01 12:13:01 +0200

    Update

--- NodeJS_SourceCode/MyNEM/Chapter_2/index.js
+++ NodeJS_SourceCode/MyNEM/Chapter_2/index.js
@@ -1,22 +1,21 @@
-const express = require("express"); //require express module
-const app = express(); //calls express function to start new Express app
-const path = require("path");
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const app = express();
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'contact.html'));
+});
+
 app.listen(3000, () => {
-  console.log("App listening on port 3000");
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(_dirname, "index.html"));
-});
-
-app.get("/about", (req, res) => {
-  // called when request to /about comes in
-  res.sendFile(path.resolve(__dirname, "about.html"));
-});
-
-app.get("/contact", (req, res) => {
-  //called when request to /contact comes
-  res.sendFile(path.resolve(__dirname, "contact.html"));
+    console.log("App listening on port 3000");
 });
