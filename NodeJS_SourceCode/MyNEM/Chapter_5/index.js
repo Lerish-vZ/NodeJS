e10eef5b3e0c94c323e12508b62f60a7d00c03a7
Lerish-vZ <lerishavz@gmail.com>
2023-09-06 12:31:48 +0200

    Update

--- NodeJS_SourceCode/MyNEM/Chapter_5/index.js
+++ NodeJS_SourceCode/MyNEM/Chapter_5/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = new express();
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true});
 
 app.set('view engine', 'ejs');
 
