dfa097e11893bb054202f560d806ad412c1d4ee4
Lerish-vZ <lerishavz@gmail.com>
2023-09-07 15:46:51 +0200

    Update

---
+++ MyNEM/Chapter_6/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const app = new express();
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1/my_database', {useNewUrlParser: true}); 
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.get('/post', (req, res) => {
+    res.render('post');
+});
+
+app.listen(3000, () => {
+    console.log("App listening on port 3000");
+});
