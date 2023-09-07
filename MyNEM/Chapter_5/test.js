cf55ef0b352eb31a56b919075fc44d3c76f18ecf
Lerish-vZ <lerishavz@gmail.com>
2023-09-07 14:18:07 +0200

    Update

--- MyNEM/Chapter_5/test.js
+++ MyNEM/Chapter_5/test.js
@@ -3,45 +3,73 @@ const BlogPost = require("./models/BlogPost");
 
 mongoose.connect("mongodb://localhost/my_database", { useNewUrlParser: true });
 
-BlogPost.create(
-  {
-    title: "The Mythbuster Guide to Saving Money on Energy Bills",
-    body: "If you have been here a long time, you might remember when I went on ITV Tonight to dispense a masterclass in saving money on energy bills. Energy-saving is one of my favourite money topics, because once you get past the boring bullet-point lists, a whole new world of thrifty nerdery opens up. You know those bullet-point lists. You start spotting them everything at this time of year. They go like this:",
-  },
-  (error, blogpost) => {
-    console.log(error, blogpost);
-  }
-);
+// BlogPost.create(
+//   {
+//     title: "The Mythbuster Guide to Saving Money on Energy Bills",
+//     body: "If you have been here a long time, you might remember when I went on ITV Tonight to dispense a masterclass in saving money on energy bills. Energy-saving is one of my favourite money topics, because once you get past the boring bullet-point lists, a whole new world of thrifty nerdery opens up. You know those bullet-point lists. You start spotting them everything at this time of year. They go like this:",
+//   },
+//   (error, blogpost) => {
+//     console.log(error, blogpost);
+//   }
+// );
 
-BlogPost.find({}, (error, blogspot) => {
-  //displays all the  documents in BlogPosts
-  console.log(error, blogspot);
-});
+BlogPost.create({
+  title: "The Mythbuster Guide to Saving Money on Energy Bills",
+  body:
+    "If you have been here a long time, you might remember when I " +
+    "went on ITV Tonight to dispense a masterclass in saving money on energy " +
+    "bills. Energy-saving is one of my favourite money topics, because once " +
+    "you get past the boring bullet-point lists, a whole new world of thrifty " +
+    "nerdery opens up. You know those bullet-point lists. You start spotting " +
+    "them everything at this time of year. They go like this:",
+})
+  .then((blogspot) => {
+    console.log("Successfully found \n" + blogspot); //\n makes new line
+  })
+  .catch((error) => {
+    console.log("Failed to find \n" + error);
+  });
+
+BlogPost.find({})
+  .then((blogpost) => {
+    console.log(blogpost);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 BlogPost.find(
   {
     title: "The Mythbuster’s Guide to Saving Money on Energy Bills",
-  },
-  (error, blogspot) => {
-    console.log(error, blogspot);
-  }
-); //query for particular title
+  })
+  .then((blogpost) => {
+    console.log(blogpost);
+  })
+  .catch((error) => {
+    console.log(error);
+  }); //query for particular title
 
 BlogPost.find(
   {
     title: /The/,
-  },
-  (error, blogspot) => {
-    console.log(error, blogspot);
-  }
-); //finds all documents with 'The' in Title. / acts like % (wildcard)
+  })
+  .then((blogpost) => {
+    console.log(blogpost);
+  })
+  .catch((error) => {
+    console.log(error);
+  }); //finds all documents with 'The' in Title. / acts like % (wildcard)
 
 var id = ""; //get the id;
 
-BlogPost.findById(id, (error, blogspot) => {
-  //find specific doc by id
-  console.log(error, blogspot);
-});
+BlogPost.findById(
+  {id})
+  .then((blogpost) => {
+    console.log(blogpost);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 BlogPost.findByIdAndUpdate(
   id,
