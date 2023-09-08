64512009409759e8f54346c7ec6742c4615af723
Lerish-vZ <lerishavz@gmail.com>
2023-09-08 14:16:07 +0200

    Update

--- MyNEM/Chapter_6/index.js
+++ MyNEM/Chapter_6/index.js
@@ -1,36 +1,40 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = new express();
-const ejs = require('ejs');
-const mongoose = require('mongoose');
+const ejs = require("ejs");
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1/my_database', {useNewUrlParser: true}); 
+mongoose.connect("mongodb://127.0.0.1/my_database", { useNewUrlParser: true });
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/about', (req, res) => {
-    res.render('about');
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
-app.get('/contact', (req, res) => {
-    res.render('contact');
+app.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
-app.get('/post', (req, res) => {
-    res.render('post');
+app.get("/post", (req, res) => {
+  res.render("post");
 });
 
-app.get('/posts/new',(req,res)=>{
-    res.render('create')
-    });
+app.get("/posts/new", (req, res) => {
+  res.render("create");
+});
+
+app.post("/posts/store", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
 
 app.listen(3000, () => {
-    console.log("App listening on port 3000");
+  console.log("App listening on port 3000");
 });
-
