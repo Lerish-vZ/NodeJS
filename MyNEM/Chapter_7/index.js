fa30e9e4c7295edbe103b5eb0ae541a6edecb5be
Lerish-vZ <lerishavz@gmail.com>
2023-09-29 14:49:55 +0200

    Update

--- MyNEM/Chapter_7/index.js
+++ MyNEM/Chapter_7/index.js
@@ -3,20 +3,23 @@ const path = require("path");
 const app = new express();
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const BlogPost = require('./models/BlogPost')
+const BlogPost = require("./models/BlogPost");
+const fileUpload = require("express-fileupload");
 
 mongoose.connect("mongodb://127.0.0.1/my_database", { useNewUrlParser: true });
 
 app.set("view engine", "ejs");
 
+app.use(fileUpload());
 app.use(express.static("public"));
 app.use(express.json()); //in post function gets data from browser via request body attribute
 app.use(express.urlencoded()); //installs body parsing middleware
 
 app.get("/", async (req, res) => {
   const blogposts = await BlogPost.find({});
-  res.render("index", { //pass back blogposts data to client browser by providing it as the 2nd argument to render
-    blogposts
+  res.render("index", {
+    //pass back blogposts data to client browser by providing it as the 2nd argument to render
+    blogposts,
   });
 });
 
@@ -29,9 +32,9 @@ app.get("/contact", (req, res) => {
 });
 
 app.get("/post/:id", async (req, res) => {
-  const blogpost = await BlogPost.findById(req.params.id)
+  const blogpost = await BlogPost.findById(req.params.id);
   res.render("post", {
-    blogpost
+    blogpost,
   });
 });
 
@@ -39,17 +42,20 @@ app.get("/posts/new", (req, res) => {
   res.render("create");
 });
 
-app.post("/posts/store", async (req, res) => { //used to create records on server
+app.post("/posts/store", async (req, res) => {
+  //used to create records on server
+  let image = req.files.image;
+  image.mv(path.resolve(__dirname, "public/img", image.name));
   //model creates a new doc with browser data
- await BlogPost.create(req.body) 
-  .then((blogpost) => {
-    res.redirect("/"); 
-  }) //req.body has the browser form data
-  
-  .catch((error) => {
-    console.log(error);
-  });
- });
+  await BlogPost.create(req.body)
+    .then((blogpost) => {
+      res.redirect("/");
+    }) //req.body has the browser form data
+
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 app.listen(3000, () => {
   console.log("App listening on port 3000");
