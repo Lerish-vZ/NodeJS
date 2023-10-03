d3cd92ad4ba88bfae4100515d204dbbcfc6c38bb
Lerish-vZ <lerishavz@gmail.com>
2023-10-03 15:12:45 +0200

    Update

--- MyNEM/Chapter_8/index.js
+++ MyNEM/Chapter_8/index.js
@@ -6,11 +6,28 @@ const mongoose = require("mongoose");
 const BlogPost = require("./models/BlogPost");
 const fileUpload = require("express-fileupload");
 
+const customMiddleWare = (req, res, next) => {
+  console.log("Custom middle ware called");
+  next();
+};
+app.use(customMiddleWare);
+
+const validateMiddleWare = (req, res, next) => {
+  if (req.files == null || req.body.title == null) {
+    return res.redirect("/posts/new");
+  }
+  next();
+};
+
+
 mongoose.connect("mongodb://127.0.0.1/my_database", { useNewUrlParser: true });
 
 app.set("view engine", "ejs");
 
 app.use(fileUpload());
+
+app.use("/posts/store", validateMiddleWare);
+
 app.use(express.static("public"));
 app.use(express.json()); //in post function gets data from browser via request body attribute
 app.use(express.urlencoded()); //installs body parsing middleware
