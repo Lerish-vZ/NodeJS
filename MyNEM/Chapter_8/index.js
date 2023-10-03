6f4f3b7a3920ed2ccd0b1d4679c4dbb1daed8dd6
Lerish-vZ <lerishavz@gmail.com>
2023-10-03 15:11:24 +0200

    Update

--- MyNEM/Chapter_8/index.js
+++ MyNEM/Chapter_8/index.js
@@ -6,21 +6,7 @@ const mongoose = require("mongoose");
 const BlogPost = require("./models/BlogPost");
 const fileUpload = require("express-fileupload");
 
-const customMiddleWare = (req, res, next) => {
-  console.log("Custom Middleware called");
-  next();
-};
-
-app.use(customMiddleWare);
-
-const validateMiddleWare = (req, res, next) => {
-  if (req.files == null || req.body.title == null) {
-    return res.redirect("/post/new");
-  }
-  next();
-};
-
-app.use("/posts/store", validateMiddleWare);
+mongoose.connect("mongodb://127.0.0.1/my_database", { useNewUrlParser: true });
 
 app.set("view engine", "ejs");
 
@@ -28,8 +14,6 @@ app.use(fileUpload());
 app.use(express.static("public"));
 app.use(express.json()); //in post function gets data from browser via request body attribute
 app.use(express.urlencoded()); //installs body parsing middleware
-
-mongoose.connect("mongodb://127.0.0.1/my_database", { useNewUrlParser: true });
 
 app.get("/", async (req, res) => {
   const blogposts = await BlogPost.find({});
@@ -60,13 +44,15 @@ app.get("/posts/new", (req, res) => {
 
 app.post("/posts/store", (req, res) => {
   let image = req.files.image;
-  image.mv(path.resolve(__dirname, "public/img", image.name), async (error) => {
-    await BlogPost.create({ ...req.body, image: "/img/" + image.name })
-      .then(res.redirect("/"))
-      .catch((error) => {
-        console.log(error);
-      });
-  });
+  image
+    .mv(path.resolve(__dirname, "public/img", image.name))
+    .then(() => BlogPost.create({ ...req.body, image: "/img/" + image.name }))
+    .then((BlogPost) => {
+      res.redirect("/");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 app.listen(3000, () => {
