8585a065ce17a383eba6f5d98388f19b0774530f
Lerish-vZ <lerishavz@gmail.com>
2023-10-03 15:44:31 +0200

    Update

--- MyNEM/Chapter_9/index.js
+++ MyNEM/Chapter_9/index.js
@@ -52,18 +52,7 @@ app.get("/post/:id", async (req, res) => {
 
 app.get('/posts/new', newPostController);
 
-app.post("/posts/store", (req, res) => {
-  let image = req.files.image;
-  image
-    .mv(path.resolve(__dirname, "public/img", image.name))
-    .then(() => BlogPost.create({ ...req.body, image: "/img/" + image.name }))
-    .then((BlogPost) => {
-      res.redirect("/");
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
+
 
 app.listen(3000, () => {
   console.log("App listening on port 3000");
