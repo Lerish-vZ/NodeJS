1f7783d15afa59b9fc77556c165501fbf2973e0a
Lerish-vZ <lerishavz@gmail.com>
2023-10-04 09:49:22 +0200

    Update

--- MyNEM/Chapter_9/index.js
+++ MyNEM/Chapter_9/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
-const path = require("path");
+// const path = require("path");
 const app = new express();
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const BlogPost = require("./models/BlogPost");
+// const BlogPost = require("./models/BlogPost");
 const fileUpload = require("express-fileupload");
 
 const newPostController = require("./controllers/newPost");
+const homeController = require("./controllers/home");
+const storePostController = require("./controllers/storePost");
+const getPostController = require("./controllers/getPost");
 
 const customMiddleWare = (req, res, next) => {
   console.log("Custom middle ware called");
@@ -33,13 +36,14 @@ app.use(express.static("public"));
 app.use(express.json()); //in post function gets data from browser via request body attribute
 app.use(express.urlencoded()); //installs body parsing middleware
 
-app.get("/", async (req, res) => {
-  const blogposts = await BlogPost.find({});
-  res.render("index", {
-    //pass back blogposts data to client browser by providing it as the 2nd argument to render
-    blogposts,
-  });
-});
+// app.get("/", async (req, res) => {
+//   const blogposts = await BlogPost.find({});
+//   res.render("index", {
+//     //pass back blogposts data to client browser by providing it as the 2nd argument to render
+//     blogposts,
+//   });
+// });
+app.get("/", homeController);
 
 // app.get("/about", (req, res) => {
 //   res.render("about");
@@ -55,25 +59,26 @@ app.get("/", async (req, res) => {
 //     blogpost,
 //   });
 // });
+app.get("/post/:id", getPostController);
 
 // app.get("/posts/new", (req, res) => {
 //   res.render("create");
 // });
+app.get("posts/new", newPostController);
 
-app.get('posts/new', newPostController);
-
-app.post("/posts/store", (req, res) => {
-  let image = req.files.image;
-  image
-    .mv(path.resolve(__dirname, "public/img", image.name))
-    .then(() => BlogPost.create({ ...req.body, image: "/img/" + image.name }))
-    .then((BlogPost) => {
-      res.redirect("/");
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
+// app.post("/posts/store", (req, res) => {
+//   let image = req.files.image;
+//   image
+//     .mv(path.resolve(__dirname, "public/img", image.name))
+//     .then(() => BlogPost.create({ ...req.body, image: "/img/" + image.name }))
+//     .then((BlogPost) => {
+//       res.redirect("/");
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// });
+app.post("/posts/store", storePostController);
 
 app.listen(3000, () => {
   console.log("App listening on port 3000");
