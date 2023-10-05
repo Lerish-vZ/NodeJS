8e34ec52690004c0fdb0aa38d25e8bf104b8f3fe
Lerish-vZ <lerishavz@gmail.com>
2023-10-05 08:28:38 +0200

    Update

--- MyNEM/Chapter_10/models/User.js
+++ MyNEM/Chapter_10/models/User.js
@@ -1,11 +1,20 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const 
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
   username: String,
   password: String
 });
+
+UserSchema.pre('save', function(next){
+  const user = this;
+
+  bcrypt.hash(use.password, 10, (error, hash) => {
+    user.password = hash;
+    next()
+  })
+})
 
 const User = mongoose.model("User", UserSchema);
 
