8501d41d95de7be0af0c4d3adf31663323e8f595
Lerish-vZ <lerishavz@gmail.com>
2023-10-05 08:21:40 +0200

    Update

--- MyNEM/Chapter_10/controllers/storeUser.js
+++ MyNEM/Chapter_10/controllers/storeUser.js
@@ -2,7 +2,6 @@ const User = require("../models/User");
 const path = require("path");
 
 module.exports = (req, res) => {
-  User.create(req.body, (error, user) => {
-    res.redirect("/");
-  });
-};
+  User.create(req.body)
+  .then(res.redirect("/"))
+ };
