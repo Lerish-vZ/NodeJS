f8f73fc4b17d3fc09c448f22eaa22082893c4ffa
Lerish-vZ <lerishavz@gmail.com>
2023-10-06 08:38:59 +0200

    Update

---
+++ MyNEM/Chapter_11/controllers/loginUser.js
@@ -0,0 +1,26 @@
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+
+module.exports = (req, res) => {
+  const { username, password } = req.body;
+
+  User.findOne({ username: username })
+    .then(user => {
+        if(user){
+        return bcrypt.compare(password, user.password)
+        .then(same => {
+            if(same) {
+                res.redirect("/");
+            } else {
+                res.redirect("/auth/login");
+            }
+        });
+        } else {
+            res.redirect("/auth/login");
+        }
+    })
+    .catch(error => {
+        console.log(error);
+        res.redirect("/auth/login");
+    });
+};
---
+++ MyNEM/Chapter_11/index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+// const path = require("path");
+const app = new express();
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+// const BlogPost = require("./models/BlogPost");
+const fileUpload = require("express-fileupload");
+const validateMiddleware = require("./middleware/validationMiddleware");
+
+const newPostController = require("./controllers/newPost");
+const homeController = require("./controllers/home");
+const storePostController = require("./controllers/storePost");
+const getPostController = require("./controllers/getPost");
+
+const newUserController = require("./controllers/newUser");
+const newUser = require("./controllers/newUser");
+const storeUserController = require("./controllers/storeUser");
+const loginController = require("./controllers/login");
+const loginUserController = require("./controllers/loginUser");
+
+// const customMiddleWare = (req, res, next) => {
+//   console.log("Custom middle ware called");
+//   next();
+// };
+// app.use(customMiddleWare);
+
+mongoose.connect("mongodb://127.0.0.1/my_database", { useNewUrlParser: true });
+
+app.set("view engine", "ejs");
+
+app.use(fileUpload());
+
+app.use(express.static("public"));
+app.use(express.json()); //in post function gets data from browser via request body attribute
+app.use(express.urlencoded()); //installs body parsing middleware
+
+app.get("/", homeController);
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.get("/post/:id", getPostController);
+
+app.get("/posts/new", newPostController);
+
+app.use("/posts/store", validateMiddleware);
+app.post("/posts/store", storePostController);
+
+app.get("/auth/register", newUserController);
+
+app.post("/users/register", storeUserController);
+
+app.get("/auth/login", loginController);
+app.post("/users/login", loginUserController);
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000");
+});
