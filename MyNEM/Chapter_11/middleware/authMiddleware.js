009a28c0563a201e3f11a5697e4c67982dbb9a1b
Lerish-vZ <lerishavz@gmail.com>
2023-10-06 09:17:05 +0200

    Update

--- MyNEM/Chapter_11/middleware/authMiddleware.js
+++ MyNEM/Chapter_11/middleware/authMiddleware.js
@@ -1,8 +1,12 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
 module.exports = (req, res, next) => {
-    User.findById(req.session.userId)
-    .catch(error => {
-        return res.redirect('/');
-    });
-}
+  User.findById(req.session.userIduserId, (error, user) => {
+    if (error || !user) return res.redirect("/");
+    next();
+  });
+  
+  // .catch(error => {
+  //     return res.redirect('/');
+  // });
+};
