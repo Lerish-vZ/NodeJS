bbba54f3fcdabf9c56e2cab6bde8b3f85d3cdb1c
Lerish-vZ <lerishavz@gmail.com>
2023-10-06 14:18:19 +0200

    Update

--- MyNEM/Chapter_12/controllers/storePost.js
+++ MyNEM/Chapter_12/controllers/storePost.js
@@ -5,6 +5,8 @@ module.exports = (req, res) => {
   let image = req.files.image;
   image.mv(path.resolve(__dirname, "public/img", image.name), async (error) => {
     await BlogPost.create({ ...req.body, image: "/img/" + image.name });
-    res.redirect("/");
+    userid: req.session.userid;
+    //
   });
+  res.redirect("/");
 };
