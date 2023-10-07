f9ebb7bb2431f0ecc2ef2fe61773cf986931a704
Lerish-vZ <lerishavz@gmail.com>
2023-10-07 11:03:48 +0200

    Update

--- MyNEM/Chapter_8/index.js
+++ MyNEM/Chapter_8/index.js
@@ -62,7 +62,7 @@ app.get("/posts/new", (req, res) => {
 app.post("/posts/store", (req, res) => {
   let image = req.files.image;
   image
-    .mv(path.resolve(__dirname, "public/img", image.name))
+    .mv(path.resolve(__dirname, '..', "public/img", image.name))
     .then(() => BlogPost.create({ ...req.body, image: "/img/" + image.name }))
     .then((BlogPost) => {
       res.redirect("/");
