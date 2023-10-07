c9d2b0e535e9f4a9b023f0666b6c1d54ebb54c41
Lerish-vZ <lerishavz@gmail.com>
2023-10-07 10:59:33 +0200

    Update

---
+++ MyNEM/Chapter_14/controllers/storeUser.js
@@ -0,0 +1,16 @@
+const User = require("../models/User");
+const path = require("path");
+
+module.exports = (req, res) => {
+  User.create(req.body)
+    .then((user) => {
+      res.redirect("/");
+    })
+    .catch((error) => {
+      // console.log(error);
+      const validationErrors = Object.keys(error.errors).map((key) => error.errors[key].message);
+      req.flash('validationErrors', validationErrors);
+      req.flash('data', req.body);
+      res.redirect("/auth/register");
+    });
+};
